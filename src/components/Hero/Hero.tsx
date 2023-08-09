7fb6c9a9fea6be7e3f7177f5e31fef04c8247899
Louise Raffray <raffraylouise@gmail.com>
2023-08-09 11:05:32 +0100

    fixed styling problems

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import '../css/Hero.css'
 
 const Hero: React.FC = () => {
     return (
-        <div className="container-section hero">
+        <div className="hero-container-section hero">
             <h1 className='name'> Louise Raffray</h1>
             <p className='job'>Junior Full Stack Software Engineer</p>
             <p className='abt'> As a newly minted Software Engineer from General Assembly, my entry into the tech industry is fueled by curiosity, adaptability, and an unyielding drive for knowledge and growth. </p>
